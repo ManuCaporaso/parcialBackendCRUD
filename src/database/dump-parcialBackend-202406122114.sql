30e3a8712ce8ae94051178b13d981063180b0dde
ManuCaporaso <manulpda@gmail.com>
2024-06-12 21:20:19 -0300

    cambios

---
+++ src/database/dump-parcialBackend-202406122114.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: parcialBackend
+-- ------------------------------------------------------
+-- Server version	11.4.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `precio` float DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `categoria` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'bananas',2000,500,'frutas'),(2,'silla escritorio',52000,200,'oficina'),(3,'manzanas',1000,400,'frutas'),(4,'lentejas',4000,250,'legumbres'),(5,'silla gamer',319000,120,'oficina');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'parcialBackend'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-12 21:14:45
--- src/router/productos.router.js
+++ src/router/productos.router.js
@@ -83,38 +83,69 @@ router.delete('/productos/:id', async (req, res) => {
     }
 });
 
-// Obtener productos ordenados
-router.get('/productos/ordenados', async (req, res) => {
-    const { criterio } = req.query;
-
+// Obtener todos los Productos Ordenados
+exports.getAllProductosOrdered = async (req, res) => {
+    const {criterio} = req.params
     try {
-        const productos = await Producto.findAll({
-            order: [[criterio, 'ASC']]
+        const Productos = await Producto.findAll();
+        switch (criterio) { 
+            case 'precio':
+                Productos.sort((a, b) => a.dataValues.precio - b.dataValues.precio);
+                break;
+            case 'nombre':
+                Productos.sort((a, b) => a.dataValues.nombre.localeCompare(b.dataValues.nombre));
+                break;
+            case 'cantidad':
+                Productos.sort((a, b) => a.dataValues.cantidad - b.dataValues.cantidad);
+                break;   
+                     
+        }  
+
+        res.status(200).json({
+            ok: true,
+            data: Productos,
+            msg: "Estos son los Productos Ordenados"
         });
-        res.status(200).json(productos);
     } catch (error) {
-        res.status(500).json({ error: 'Error al obtener los productos ordenados' });
+        console.log(error);
+        res.status(500).json({ error: 'Error al obtener los Productos' });
     }
-});
+};
 
-// Obtener productos filtrados
-router.get('/productos/filtrados', async (req, res) => {
-    const { precio, categoria } = req.query;
-    let whereClause = {};
-
-    if (precio) {
-        whereClause.precio = { [Op.gt]: precio };
-    }
-    if (categoria) {
-        whereClause.categoria = categoria;
-    }
+// Obtener todos los Productos Filtrados
+exports.getAllProductosFiltered = async (req, res) => {
+    const {criterio} = req.params
+    const {valor} = req.query
 
     try {
-        const productos = await Producto.findAll({ where: whereClause });
-        res.status(200).json(productos);
+        let Productos = await Producto.findAll();
+
+        switch (criterio) { 
+            case 'precioMayor':
+          
+                Productos = Productos.filter(p=>p.dataValues.precio>=valor)
+                break;
+            case 'categoria':
+                Productos = Productos.filter(p=>p.dataValues.categoria==valor)
+                break;   
+                        
+        }  
+        if (Productos.length>0) 
+            res.status(200).json({
+                ok: true,
+                data: Productos,
+                msg: "Estos son los Productos de acuerdo al filtro"
+            });
+        else 
+            res.status(404).json({
+                ok: false,
+                msg: "No se encontraron productos con el criterio enviado"
+            }); 
+
     } catch (error) {
-        res.status(500).json({ error: 'Error al obtener los productos filtrados' });
+
+        res.status(500).json({ error: 'Error al obtener los Productos' });
     }
-});
+};
 
 module.exports = router;
